3c73b3b7fd9be13a5f2fe889dbe63014be87c9a0
Domagoj Margan <margan.domagoj@gmail.com>
2017-11-09 15:01:47 +0000

    Added datatypes files

---
+++ src/core/datatypes.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dm1515 on 09/11/17.
+//
+
+#include "datatypes.h"
---
+++ src/core/datatypes.h
@@ -0,0 +1,8 @@
+//
+// Created by dm1515 on 09/11/17.
+//
+
+#ifndef KAPPA_DATATYPES_H
+#define KAPPA_DATATYPES_H
+
+#endif //KAPPA_DATATYPES_H
