9cf0ae8b635702bb54e1bb6e3cdc82d27d0a63cd
Domagoj Margan <margan.domagoj@gmail.com>
2017-12-01 00:51:47 +0000

    Added a simple constructor for graphs

--- src/core/graph.cpp
+++ src/core/graph.cpp
@@ -3,3 +3,18 @@
 //
 
 #include "graph.h"
+
+Graph::Graph(int v_num) {
+    edges.assign(v_num, std::vector<int>(0, 0));
+}
+
+edge_array Graph::get_edge_array() {
+    for (std::size_t i=0; i<edges.size(); i++){
+        for (std::size_t j=0; j<edges[i].size(); j++){
+            std::cout << edges[i][j] << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    return edge_array();
+}
--- src/core/graph.h
+++ src/core/graph.h
@@ -5,9 +5,17 @@
 #ifndef KAPPA_GRAPH_H
 #define KAPPA_GRAPH_H
 
+#include <vector>
+#include <iostream>
+
+typedef std::vector<std::vector<int>> edge_array;
 
 class Graph {
-
+private:
+    edge_array edges;
+public:
+    explicit Graph(int);
+    edge_array get_edge_array();
 };
 
 
