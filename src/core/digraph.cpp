cf980139de43725489e24ee5a5a8ff13224d17f2
Domagoj Margan <margan.domagoj@gmail.com>
2017-12-12 16:19:00 +0000

    Added files for digraph class

---
+++ src/core/digraph.cpp
@@ -0,0 +1,5 @@
+//
+// Created by dm1515 on 12/12/17.
+//
+
+#include "digraph.h"
---
+++ src/core/digraph.h
@@ -0,0 +1,14 @@
+//
+// Created by dm1515 on 12/12/17.
+//
+
+#ifndef KAPPA_DIGRAPH_H
+#define KAPPA_DIGRAPH_H
+
+
+class digraph {
+
+};
+
+
+#endif //KAPPA_DIGRAPH_H
