800e500ef18b78f979a95196a687d44c43782f8b
Domagoj Margan <margan.domagoj@gmail.com>
2017-12-12 23:36:54 +0000

    Renamed vertex_vector to graph_vector type and changed some ints to unsigned int types in graph class header

--- src/core/graph.h
+++ src/core/graph.h
@@ -4,24 +4,27 @@
 #include <vector>
 #include "vertex.h"
 
-typedef std::vector<uvertex> vertex_vector;
+typedef std::vector<uvertex> graph_vector;
 
 class Graph {
 private:
     unsigned int max_vertex_number; // Set a limit for the maximum possible number of vertices to fit in the graph.
-    vertex_vector topology;
 public:
+    graph_vector topology;
+    unsigned int order;
+    unsigned int size;
+
     explicit Graph(unsigned int);
-    vertex_vector get_vertex_vector(); // Return the vertex_vector data structure.
+    graph_vector get_graph_vector(); // Return the graph_vector data structure.
     void print_edges(); // (use just for testing)
     void add_edge(unsigned int, unsigned int); // Add an edge between two vertices.
     void remove_edge(unsigned int, unsigned int); // Remove the edge between two vertices.
     bool has_edge(unsigned int, unsigned int); // Return true of the edge is in the graph.
-    int in_degree(unsigned int); // Return the in-degree of a vertex.
-    int out_degree(unsigned int); // Return the out-degree of a vertex.
-    int degree(unsigned int); // Return the degree of a vertex.
-    int order(); // Return the number of vertices.
-    int size(); // Return the number of edges.
+    unsigned int in_degree(unsigned int); // Return the in-degree of a vertex.
+    unsigned int out_degree(unsigned int); // Return the out-degree of a vertex.
+    unsigned int degree(unsigned int); // Return the degree of a vertex.
+    unsigned int order(); // Return the number of vertices.
+    unsigned int size(); // Return the number of edges.
 };
 
 #endif //KAPPA_GRAPH_H
