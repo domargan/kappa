5d812fd66dca72efbac1aa273ee9df5705120097
Domagoj Margan <margan.domagoj@gmail.com>
2017-12-12 16:17:58 +0000

    Added files for vertex struct

---
+++ src/core/vertex.h
@@ -0,0 +1,21 @@
+#ifndef KAPPA_VERTEX_H
+#define KAPPA_VERTEX_H
+
+typedef std::vector<unsigned int> neighbors_vector;
+
+typedef struct uvertex {
+    //unsigned int id;
+    neighbors_vector* neighbors;
+    int degree;
+    float state;
+};
+
+typedef struct dvertex {
+    //unsigned int id;
+    neighbors_vector* in_neighbors;
+    neighbors_vector* out_neighbors;
+    int in_degree;
+    int out_degree;
+    float state;
+};
+#endif //KAPPA_VERTEX_H
