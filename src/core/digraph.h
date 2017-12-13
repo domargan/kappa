f62103a54700537ced68e279d96f8a311cc08532
Domagoj Margan <margan.domagoj@gmail.com>
2017-12-13 00:41:18 +0000

    Added digraph class declaration

--- src/core/digraph.h
+++ src/core/digraph.h
@@ -1,14 +1,31 @@
-//
-// Created by dm1515 on 12/12/17.
-//
-
 #ifndef KAPPA_DIGRAPH_H
 #define KAPPA_DIGRAPH_H
 
+#include <vector>
+#include "vertex.h"
 
-class digraph {
+typedef std::vector<dvertex> graph_vector;
 
+class Digraph {
+private:
+    unsigned int max_vertex_number; // Set a limit for the maximum possible number of vertices to fit in the graph.
+    graph_vector topology;
+    unsigned int order;
+    unsigned int size;
+public:
+    explicit Digraph(unsigned int);
+    graph_vector get_graph_vector(); // Return the graph_vector data structure.
+    void print_edges(); // (use just for testing)
+    void add_edge(unsigned int, unsigned int); // Add an edge between two vertices.
+    void remove_edge(unsigned int, unsigned int); // Remove the edge between two vertices.
+    bool has_edge(unsigned int, unsigned int); // Return true of the edge is in the graph.
+    neighbors_vector get_in_neighborhood(unsigned int); // Return the vector of incoming neighbors of a vertex.
+    neighbors_vector get_out_neighborhood(unsigned int); // Return the vector of outcoming neighbors of a vertex.
+    unsigned int get_in_degree(unsigned int); // Return the in-degree of a vertex.
+    unsigned int get_out_degree(unsigned int); // Return the out-degree of a vertex.
+    unsigned int get_degree(unsigned int); // Return the degree of a vertex.
+    unsigned int get_order(); // Return the number of vertices.
+    unsigned int get_size(); // Return the number of edges.
 };
-
 
 #endif //KAPPA_DIGRAPH_H
