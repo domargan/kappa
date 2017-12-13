cb7fc528706d3ef07fdde57705350340f7eed8fc
Domagoj Margan <margan.domagoj@gmail.com>
2017-12-13 00:42:16 +0000

    Removed member function declarations from vertex header file -- now in graph class headers

--- src/core/vertex.h
+++ src/core/vertex.h
@@ -8,12 +8,6 @@ typedef struct uvertex {
     neighbors_vector* neighbors;
     int degree;
     float state;
-    // get member functions out!
-    void add_neighbor(unsigned int, unsigned int);
-    neighbors_vector get_neighbors();
-    int get_degree();
-    void write_state(float);
-    float get_state();
 };
 
 typedef struct dvertex {
@@ -23,16 +17,6 @@ typedef struct dvertex {
     int in_degree;
     int out_degree;
     float state;
-    // get member functions out!
-    void add_in_neighbor(unsigned int);
-    void add_out_neighbor(unsigned int);
-    neighbors_vector get_in_neighbors();
-    neighbors_vector get_out_neighbors();
-    int get_in_degree();
-    int get_out_degree();
-    int get_degree();
-    void write_state(float);
-    float get_state();
 };
 
 
