6b68f65250e434a9795384afc2fbecd1c9bee0bf
Domagoj Margan <margan.domagoj@gmail.com>
2017-12-18 00:30:21 +0000

    TEMP: neighbor vectors are not pointers anymore

--- src/core/vertex.h
+++ src/core/vertex.h
@@ -5,18 +5,20 @@ typedef std::vector<unsigned int> neighbors_vector;
 
 typedef struct uvertex {
     //unsigned int id;
-    neighbors_vector* neighbors;
+    neighbors_vector neighbors;
     int degree;
     float state;
+    float state_temp;
 };
 
 typedef struct dvertex {
     //unsigned int id;
-    neighbors_vector* in_neighbors;
-    neighbors_vector* out_neighbors;
+    neighbors_vector in_neighbors;
+    neighbors_vector out_neighbors;
     int in_degree;
     int out_degree;
     float state;
+    float state_temp;
 };
 
 
