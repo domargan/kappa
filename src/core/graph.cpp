d229b6d49b4fcbcab04199e87acdc1393b8fe382
Domagoj Margan <margan.domagoj@gmail.com>
2017-12-18 00:22:01 +0000

    Changed graph class so tt now represents only undirected graphs

--- src/core/graph.cpp
+++ src/core/graph.cpp
@@ -1,27 +1,39 @@
+#include <iostream>
+#include <algorithm>
+
 #include "graph.h"
-#include "algorithm"
+#include "vertex.h"
 
 Graph::Graph(unsigned int v_num) {
     std::cout << "Graph constructor called.\n" << std::endl;
 
     max_vertex_number = v_num;
     // std::vector.assign() has O(n) in this case.
-    edges.assign(max_vertex_number, std::vector<unsigned int>());
+    //topology.assign(max_vertex_number, uvertex());
+    for (int i=0; i<max_vertex_number; i++) {
+        uvertex uv;
+        topology.push_back(uv);
+    }
 
     std::cout << "Graph structure initialized with " << v_num << " vertex entries.\n" << std::endl;
+
+    order = 0;
+    size = 0;
 }
 
-edge_vector Graph::get_edge_vector() {
-    return edge_vector();
+graph_vector Graph::get_graph_vector() {
+    return topology;
 }
 
-void Graph::print_edge_vector() {
+void Graph::print_edges() {
     //for (auto &edge : edges) {
     //    for (int i : edge) {
     for (int i=0; i<max_vertex_number; i++){
-        std::cout << "v" << i << ": ";
-        for (auto j : edges[i]){
-            std::cout << j << " ";
+        std::cout << "v" << i << ": " << std::endl;
+        //for (auto j : topology[i].neighbors){
+        for (int j = 0; j < topology[i].neighbors.size(); j++) {
+            std::cout << topology[i].neighbors[j] << " " << std::endl;
+            //std::cout << j << std::endl;
         }
         std::cout << std::endl;
     }
@@ -30,37 +42,53 @@ void Graph::print_edge_vector() {
 
 bool Graph::has_edge(unsigned int src_v, unsigned int dst_v){
     // std::find() has a worst-case time of O(n) in the distance between first and last.
-    return std::find(edges[src_v].begin(), edges[src_v].end(), dst_v) != edges[src_v].end();
+    return std::find(topology[src_v].neighbors.begin(), topology[src_v].neighbors.end(), dst_v)
+           != topology[src_v].neighbors.end();
 }
 
 void Graph::add_edge(unsigned int src_v, unsigned int dst_v) {
     // std::vector.push_back() has a O(1) amortized time.
     // Reallocation may happen.
     // If a reallocation happens, the reallocation is O(n).
-    edges[src_v].push_back(dst_v);
+    topology[src_v].neighbors.push_back(dst_v);
+    //topology[src_v];
 }
 
 void Graph::remove_edge(unsigned int src_v, unsigned int dst_v) {
     // Erase-remove has a worst-case time of O(n).
-    edges[src_v].erase(std::remove(edges[src_v].begin(), edges[src_v].end(), dst_v), edges[src_v].end());;
+    //edges[src_v].erase(std::remove(edges[src_v].begin(), edges[src_v].end(), dst_v), edges[src_v].end());;
+    topology[src_v].neighbors.erase(std::remove(topology[src_v].neighbors.begin(), topology[src_v].neighbors.end(), dst_v), topology[src_v].neighbors.end());
 }
 
-int Graph::in_degree(unsigned int) {
+
+neighbors_vector Graph::get_neighborhood(unsigned int v) {
+    return topology[v].neighbors;
+}
+
+unsigned int Graph::get_degree(unsigned int v) {
+    return get_neighborhood(v).size();
+}
+
+unsigned int Graph::get_order() {
     return 0;
 }
 
-int Graph::out_degree(unsigned int) {
+unsigned int Graph::get_size() {
     return 0;
 }
 
-int Graph::degree(unsigned int) {
-    return 0;
+void Graph::increment_order() {
+
 }
 
-int Graph::order() {
-    return 0;
+void Graph::decrement_order() {
+
 }
 
-int Graph::size() {
-    return 0;
+void Graph::increment_size() {
+
+}
+
+void Graph::decrement_size() {
+
 }
--- src/core/graph.h
+++ src/core/graph.h
@@ -9,26 +9,31 @@ typedef std::vector<uvertex> graph_vector;
 class Graph {
 private:
     unsigned int max_vertex_number; // Set a limit for the maximum possible number of vertices to fit in the graph.
-    graph_vector topology;
-    unsigned int order;
-    unsigned int size;
-        /*
-    void increment_order();
-    void decrement_order();
-    void increment_size();
-    void decrement_size();
-     */
+
+    graph_vector topology; // Graph topology data + some metadata.
+
+    unsigned int order; // Current number of vertices in the graph.
+    unsigned int size; // Current number of edges in the graph.
 public:
-    explicit Graph(unsigned int);
+    explicit Graph(unsigned int); // Graph constructor
+
     graph_vector get_graph_vector(); // Return the graph_vector data structure.
     void print_edges(); // (use just for testing)
+
     void add_edge(unsigned int, unsigned int); // Add an edge between two vertices.
     void remove_edge(unsigned int, unsigned int); // Remove the edge between two vertices.
     bool has_edge(unsigned int, unsigned int); // Return true of the edge is in the graph.
+
     neighbors_vector get_neighborhood(unsigned int); // Return the vector of neighbors of a vertex.
     unsigned int get_degree(unsigned int); // Return the degree of a vertex.
+
     unsigned int get_order(); // Return the number of vertices.
     unsigned int get_size(); // Return the number of edges.
+
+    void increment_order(); // Increase the value for number of vertices by 1.
+    void decrement_order(); // Decrease the value for number of vertices by 1.
+    void increment_size(); // Increase the value for number of edges by 1.
+    void decrement_size(); // Increase the value for number of edges by 1.
 };
 
 #endif //KAPPA_GRAPH_H
