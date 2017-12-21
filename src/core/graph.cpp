b02131b7f818cae5f40b23b55b9eb24d74281da2
Domagoj Margan <margan.domagoj@gmail.com>
2017-12-21 19:32:58 +0000

    Added checks for edge existance

--- src/core/graph.cpp
+++ src/core/graph.cpp
@@ -28,7 +28,6 @@ graph_vector Graph::get_graph_vector() {
     return topology;
 }
 
-
 void Graph::print_edges() {
     for (std::vector<uvertex>::size_type v = 0; v != topology.size(); v++) {
         std::cout << "v" << v << ": " << std::endl;
@@ -47,21 +46,27 @@ bool Graph::has_edge(unsigned int src_v, unsigned int dst_v) {
 }
 
 void Graph::add_edge(unsigned int src_v, unsigned int dst_v) {
-    // TODO: Check if the edge exists already before adding it.
-
-    // std::vector.push_back() has a O(1) amortized time.
-    // Reallocation may happen.
-    // If a reallocation happens, the reallocation is O(n).
-    topology[src_v].neighbors->push_back(dst_v);
+    if (!has_edge(src_v, dst_v)) {
+        // std::vector.push_back() has a O(1) amortized time.
+        // Reallocation may happen.
+        // If a reallocation happens, the reallocation is O(n).
+        topology[src_v].neighbors->push_back(dst_v);
+        topology[dst_v].neighbors->push_back(src_v);
+    }
 }
 
 void Graph::remove_edge(unsigned int src_v, unsigned int dst_v) {
-    // Erase-remove has a worst-case time of O(n).
-    topology[src_v].neighbors->erase
-            (std::remove(topology[src_v].neighbors->begin(), topology[src_v].neighbors->end(), dst_v),
-             topology[src_v].neighbors->end());
-}
+    if (has_edge(src_v, dst_v)) {
+        // Erase-remove has a worst-case time of O(n).
+        topology[src_v].neighbors->erase
+                (std::remove(topology[src_v].neighbors->begin(), topology[src_v].neighbors->end(), dst_v),
+                 topology[src_v].neighbors->end());
 
+        topology[dst_v].neighbors->erase
+                (std::remove(topology[dst_v].neighbors->begin(), topology[dst_v].neighbors->end(), src_v),
+                 topology[dst_v].neighbors->end());
+    }
+}
 
 neighbors_vector Graph::get_neighborhood(unsigned int v) {
     return *topology[v].neighbors;
