f5b3062139cba68139eb3ce0cc24677b23447170
Domagoj Margan <margan.domagoj@gmail.com>
2017-12-26 18:53:51 +0000

    Added functions for building static graphs from edge arrays

--- src/core/build_graph_static.cpp
+++ src/core/build_graph_static.cpp
@@ -1,5 +1,28 @@
-//
-// Created by domargan on 21/12/17.
-//
-
+#include <algorithm>
+#include <set>
+#include "read_tsv_disk.h"
 #include "build_graph_static.h"
+
+Graph edge_array_to_graph(raw_edge_array& edge_array) {
+    unsigned int v_num = unique_vertex_count(extract_vertices(edge_array));
+
+    Graph graph = Graph(v_num);
+
+    for(auto edge : edge_array) {
+        graph.add_edge(edge[0], edge[1]);
+    }
+
+    return graph;
+}
+
+Digraph edge_array_to_digraph(raw_edge_array& edge_array) {
+    unsigned int v_num = unique_vertex_count(extract_vertices(edge_array));
+
+    Digraph digraph = Digraph(v_num);
+
+    for(auto edge : edge_array) {
+        digraph.add_edge(edge[0], edge[1]);
+    }
+
+    return digraph;
+}
--- src/core/build_graph_static.h
+++ src/core/build_graph_static.h
@@ -1,8 +1,10 @@
-//
-// Created by domargan on 21/12/17.
-//
-
 #ifndef KAPPA_BUILD_GRAPH_STATIC_H
 #define KAPPA_BUILD_GRAPH_STATIC_H
+
+#include "graph.h"
+#include "digraph.h"
+
+Graph edge_array_to_graph(raw_edge_array&);
+Digraph edge_array_to_digraph(raw_edge_array&);
 
 #endif //KAPPA_BUILD_GRAPH_STATIC_H
