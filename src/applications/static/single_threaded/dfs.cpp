825d42e4b0ac640207c95ef2ab23b7ed78d1abbc
Domagoj Margan <margan.domagoj@gmail.com>
2018-01-06 20:24:44 +0000

    Implemented DFS-based single-threaded static connected components

--- src/applications/static/single_threaded/dfs.cpp
+++ src/applications/static/single_threaded/dfs.cpp
@@ -4,6 +4,8 @@
 
 #include "dfs.h"
 
+// TODO: Use templates instead of function overloading
+
 void dfs(Digraph* g, unsigned int v) {
     std::cout << "Visiting vertex " << v << "..." << std::endl;
     g->update_state(v, 1);
@@ -16,7 +18,26 @@ void dfs(Digraph* g, unsigned int v) {
     }
 }
 
+void dfs(Graph* g, unsigned int v) {
+    std::cout << "Visiting vertex " << v << "..." << std::endl;
+    g->update_state(v, 1);
+    g->finalize_state(v);
+
+    for(auto neighbor : g->get_neighborhood(v)) {
+        if(g->get_state(neighbor) != 1) {
+            dfs(g, neighbor);
+        }
+    }
+}
+
 void dfs_compute(Digraph* g, unsigned int v) {
+    std::cout << "Starting DFS computations..." << std::endl;
+    if(g->has_vertex(v)){
+        dfs(g, v);
+    }
+}
+
+void dfs_compute(Graph* g, unsigned int v) {
     std::cout << "Starting DFS computations..." << std::endl;
     if(g->has_vertex(v)){
         dfs(g, v);
--- src/applications/static/single_threaded/dfs.h
+++ src/applications/static/single_threaded/dfs.h
@@ -1,7 +1,11 @@
 #ifndef KAPPA_DFS_H
 #define KAPPA_DFS_H
 
+#include "graph.h"
 #include "digraph.h"
+
+void dfs(Graph*, unsigned int);
+void dfs_compute(Graph*, unsigned int);
 
 void dfs(Digraph*, unsigned int);
 void dfs_compute(Digraph*, unsigned int);
