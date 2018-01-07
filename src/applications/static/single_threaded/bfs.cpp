be65c210e07104651caaefcfac3bda8fccbf8ae2
Domagoj Margan <margan.domagoj@gmail.com>
2018-01-07 14:37:22 +0000

    Implemented single-threaded static BFS

---
+++ src/applications/static/single_threaded/bfs.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <list>
+
+#include "bfs.h"
+
+// TODO: Use templates instead of function overloading
+
+void bfs(Graph* g, unsigned int v){
+    std::cout << "Starting BFS with source vertex " << v << "..." << std::endl;
+
+    std::list<unsigned int> bfs_queue;
+
+    g->update_state(v, 1);
+    g->finalize_state(v);
+
+    bfs_queue.push_back(v);
+
+    while(!bfs_queue.empty()){
+        v = bfs_queue.front();
+        //std::cout << v << " ";
+        bfs_queue.pop_front();
+
+        for(auto neighbor : g->get_neighborhood(v)) {
+            if(g->get_state(neighbor) != 1) {
+                g->update_state(neighbor, 1);
+                g->finalize_state(neighbor);
+                bfs_queue.push_back(neighbor);
+            }
+        }
+    }
+}
+
+void bfs(Digraph* g, unsigned int v){
+    std::cout << "Starting BFS with source vertex " << v << "..." << std::endl;
+
+    std::list<unsigned int> bfs_queue;
+
+    g->update_state(v, 1);
+    g->finalize_state(v);
+
+    bfs_queue.push_back(v);
+
+    while(!bfs_queue.empty()){
+        v = bfs_queue.front();
+        //std::cout << v << " ";
+        bfs_queue.pop_front();
+
+        for(auto neighbor : g->get_out_neighborhood(v)) {
+            if(g->get_state(neighbor) != 1) {
+                g->update_state(neighbor, 1);
+                g->finalize_state(neighbor);
+                bfs_queue.push_back(neighbor);
+            }
+        }
+    }
+}
---
+++ src/applications/static/single_threaded/bfs.h
@@ -0,0 +1,10 @@
+#ifndef KAPPA_BFS_H
+#define KAPPA_BFS_H
+
+#include "graph.h"
+#include "digraph.h"
+
+void bfs(Graph*, unsigned int);
+void bfs(Digraph*, unsigned int);
+
+#endif //KAPPA_BFS_H
