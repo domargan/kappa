ecdd9b3fb5152c68720aef2602cba4d050f37948
Domagoj Margan <margan.domagoj@gmail.com>
2018-02-16 00:57:18 +0000

    Added ingestion folder and naive update files

---
+++ src/ingestion/naive_update_from_tsv.cpp
@@ -0,0 +1,5 @@
+#include "naive_update_from_tsv.h"
+
+// This should be just a hacky implementation that
+// does additions of edges read from a given tsv file.
+// A proper implementation should have separated tsv/stream ingestion and graph update parts.
