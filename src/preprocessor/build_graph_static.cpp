7c5eaab00b52cc82715eae3522565ef147402244
Domagoj Margan <margan.domagoj@gmail.com>
2018-02-19 02:01:41 +0000

    Added init state parameter

--- src/preprocessor/build_graph_static.cpp
+++ src/preprocessor/build_graph_static.cpp
@@ -3,10 +3,10 @@
 #include "read_tsv_disk.h"
 #include "build_graph_static.h"
 
-Graph edge_array_to_graph(raw_edge_array& edge_array) {
+Graph edge_array_to_graph(raw_edge_array& edge_array, double init_state) {
     unsigned int v_num = unique_vertex_count(extract_vertices(edge_array));
 
-    Graph graph = Graph(v_num);
+    Graph graph = Graph(v_num, init_state);
 
     for(auto edge : edge_array) {
         graph.add_edge(edge[0], edge[1]);
@@ -15,10 +15,10 @@ Graph edge_array_to_graph(raw_edge_array& edge_array) {
     return graph;
 }
 
-Digraph edge_array_to_digraph(raw_edge_array& edge_array) {
+Digraph edge_array_to_digraph(raw_edge_array& edge_array, double init_state) {
     unsigned int v_num = unique_vertex_count(extract_vertices(edge_array));
 
-    Digraph digraph = Digraph(v_num);
+    Digraph digraph = Digraph(v_num, init_state);
 
     for(auto edge : edge_array) {
         digraph.add_edge(edge[0], edge[1]);
