9e9b6994831c725cea740cf7a5bd4bf73d170062
Domagoj Margan <margan.domagoj@gmail.com>
2018-02-19 03:23:29 +0000

    Updated init.cpp

--- src/init.cpp
+++ src/init.cpp
@@ -13,11 +13,12 @@
 int main() {
     std::cout << "Launching Kappa...\n" << std::endl;
 
-    std::string dataset = "/home/domargan/test.edgelist";
-    unsigned long core_size = 20;
-    unsigned long chunks_size = 5;
+    std::string dataset = "/home/dm1515/data/twitter-2010-shuffled.txt";
 
     unsigned long lines = number_of_lines(dataset);
+
+    unsigned long core_size = (lines * 30) / 100; // Core graph is 30% of the entire dataset
+    unsigned long chunks_size = (lines * 10) / 100; // Updates are processed in chunks of 10% of the dataset
 
     std::vector<unsigned long> split = dataset_split_by_size(lines, core_size, chunks_size);
 
@@ -26,7 +27,7 @@ int main() {
 
     unsigned int max_vertex_num = unique_vertex_count(edges);
 
-    double init_state = 1.0 / max_vertex_num; // for PageRank
+    double init_state = 1.0 / max_vertex_num; // init state for PageRank
     Digraph g = Digraph(max_vertex_num, init_state);
 
     naive_incremental_compute_tsv(pr_compute,
