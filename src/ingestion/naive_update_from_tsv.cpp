79cc2476d7173d864c203b655413f47d6698804b
Domagoj Margan <margan.domagoj@gmail.com>
2018-02-19 01:46:16 +0000

    Deleted some unused files

--- src/ingestion/naive_update_from_tsv.cpp
+++
@@ -1,5 +0,0 @@
-#include "naive_update_from_tsv.h"
-
-// This should be just a hacky implementation that
-// does additions of edges read from a given tsv file.
-// A proper implementation should have separated tsv/stream ingestion and graph update parts.
