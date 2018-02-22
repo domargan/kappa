c3dbe69ab4972e081e1161005388db1069305314
Domagoj Margan <margan.domagoj@gmail.com>
2018-02-22 17:54:00 +0000

    Added small util function for dumping vertex results to a file (just for testing the correctness of computations

---
+++ src/experiments/utils/dump_vertex_states.cpp
@@ -0,0 +1,19 @@
+#include <digraph.h>
+#include <fstream>
+#include <iomanip>
+#include "dump_vertex_states.h"
+
+void dump_vertex_states(Digraph *g, const std::string &file_name){
+    std::ofstream output_file;
+    output_file.open(file_name);
+
+    uint32_t max_order = g->get_max_order();
+
+    for(uint32_t i=0; i<max_order; i++) {
+        if(g->has_vertex(i)){
+            output_file << std::fixed << std::setprecision(5) << g->get_state(i) << "\n";
+        }
+    }
+
+    output_file.close();
+}
---
+++ src/experiments/utils/dump_vertex_states.h
@@ -0,0 +1,8 @@
+#ifndef KAPPA_DUMP_VERTEX_STATES_H
+#define KAPPA_DUMP_VERTEX_STATES_H
+
+#include <digraph.h>
+
+void dump_vertex_states(Digraph *, const std::string &);
+
+#endif //KAPPA_DUMP_VERTEX_STATES_H
