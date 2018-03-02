8c76671a4d96fb80111d71472285f6848c7d1a8b
Domagoj Margan <margan.domagoj@gmail.com>
2018-03-02 22:38:03 +0000

    Init...

--- src/init.cpp
+++ src/init.cpp
@@ -4,26 +4,25 @@
 #include <stack>
 #include <utils/dump_vertex_states.h>
 
-#include "digraph.h"
 #include "naive_incremental_compute_tsv.h"
 #include "dataset_split.h"
 #include "pagerank.h"
 
-#include "dataset_split_hardcoded.h"
-
 int main() {
     std::cout << "Launching Kappa...\n" << std::endl;
 
+    //std::string dataset = "/home/dm1515/data/test_data.edgelist";
     //std::string dataset = "/home/dm1515/data/twitter-2010-shuffled.txt";
-    //std::string dataset = "/home/dm1515/data/higgs-social_network-shuffled.edgelist";
-    std::string dataset = "/home/dm1515/data/higgs-social_network.edgelist";
+    //std::string dataset = "/home/dm1515/data/higgs-social_network.edgelist";
+    std::string dataset = "/home/dm1515/data/higgs-social_network-shuffled.edgelist";
+    //std::string dataset = "/home/dm1515/data/higgs-social_network-shuffled-head1M";
 
     uint32_t lines = number_of_lines(dataset);
 
-    //uint32_t core_size = 3000000; // Core graph is built from 3M updates
-    //uint32_t chunks_size = 1000000; // Updates are processed in chunks of 1M
+    uint32_t core_size = 1000000;
+    uint32_t chunks_size = 1000000;
 
-    //std::vector<uint32_t> split = dataset_split_by_size(lines, core_size, chunks_size);
+    std::vector<uint32_t> split = dataset_to_batches(1, lines, lines, core_size, chunks_size);
 
     raw_edge_array_t edges;
     edges = tsv_to_edges(dataset, ' ');
@@ -36,8 +35,7 @@ int main() {
     naive_incremental_compute_tsv(pr_compute,
                                   &g,
                                   edges,
-                                  SAMPLE_SPLIT_SINGLE_FINER,
-                                  lines);
+                                  split);
 
     //dump_vertex_states(&g, "results.txt");
 
