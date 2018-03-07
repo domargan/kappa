1847172ba903c149b0c5120b61127bfed7d67a04
Domagoj Margan <margan.domagoj@gmail.com>
2018-03-07 17:58:32 +0000

    We can now build a graph from a particular line set in tsv file

--- src/preprocessor/read_from_disk/tsv_to_graph.cpp
+++ src/preprocessor/read_from_disk/tsv_to_graph.cpp
@@ -5,7 +5,7 @@
 #include "tsv_to_graph.h"
 #include "digraph.h"
 
-Digraph tsv_to_digraph(const std::string &tsv_file, char separator, uint32_t v_num, state_t init_state) {
+Digraph tsv_to_digraph(const std::string &tsv_file, char separator, uint32_t beggining_line, uint32_t end_line, uint32_t v_num, state_t init_state) {
     Digraph digraph = Digraph(v_num, init_state);
 
     std::fstream fs;
@@ -16,20 +16,26 @@ Digraph tsv_to_digraph(const std::string &tsv_file, char separator, uint32_t v_n
 
         std::string line;
 
+        uint32_t line_counter = 0;
+
         while (getline(fs, line)) {
-            std::stringstream sep(line);
-            std::string vertex;
+            line_counter++;
 
-            std::vector<uint32_t> vertex_pair;
+            if(line_counter >= beggining_line && line_counter <= end_line) {
+                std::stringstream sep(line);
+                std::string vertex;
 
-            // TODO: edges needs only 2 values per field, simplify with an 1(vector)x2(array) structure
-            while (getline(sep, vertex, separator)) {
-                vertex_pair.push_back(static_cast<uint32_t &&>(stoi(vertex)));
+                std::vector<uint32_t> vertex_pair;
+
+                // TODO: edges needs only 2 values per field, simplify with an 1(vector)x2(array) structure
+                while (getline(sep, vertex, separator)) {
+                    vertex_pair.push_back(static_cast<uint32_t &&>(stoi(vertex)));
+                }
+
+                digraph.add_edge(vertex_pair.at(0), vertex_pair.at(1));
+
+                vertex_pair.clear();
             }
-
-            digraph.add_edge(vertex_pair.at(0), vertex_pair.at(1));
-
-            vertex_pair.clear();
         }
     }
 
