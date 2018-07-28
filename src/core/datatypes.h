5f29a5b9fe1297ad189f79f60b9c4c4a2fb5a9c0
Domagoj Margan <margan.domagoj@gmail.com>
2018-07-28 23:22:02 +0100

    Added scheduler files and modified datatypes

--- src/core/datatypes.h
+++ src/core/datatypes.h
@@ -9,4 +9,11 @@ typedef uint32_t graph_size_t;
 
 typedef float state_t;
 
+typedef uint32_t task_id_t;
+
+typedef enum {COMPUTE, UPDATE} task_type_t;
+
+typedef uint32_t timestamp_ms_t;
+typedef uint32_t timestamp_logical_t;
+
 #endif //KAPPA_DATATYPES_H
---
+++ src/core/scheduler.cpp
@@ -0,0 +1,33 @@
+#include "scheduler.h"
+
+void Scheduler::submit_task_to_waiting(TaskInterface) {
+
+}
+
+void Scheduler::submit_task_to_runnable(TaskInterface) {
+
+}
+
+void Scheduler::move_task_to_runnable(TaskInterface) {
+
+}
+
+void Scheduler::update_dependencies_global() {
+
+}
+
+void Scheduler::update_dependencies(task_id) {
+
+}
+
+void Scheduler::check_dependency(task_id) {
+
+}
+
+void Scheduler::notify_dependants(task_id) {
+
+}
+
+Scheduler::Scheduler(ThreadPool *) {
+
+}
