19dacb9c5f6edf7e01d4a3232a5d91af1e62d00c
Luca Campese <me@campesel.net>
2018-08-13 15:14:15 +0100

    Delete unused files

--- src/experiments/update_propagation/pagerank_propagation.h
+++
@@ -1,9 +0,0 @@
-#ifndef KAPPA_PAGERANK_H
-#define KAPPA_PAGERANK_H
-
-const float DEFAULT_DAMPING_FACTOR = 0.85;
-
-void init_pr_values_propagation(Digraph*);
-void pr_compute_single_vertex_propagation(vertex_id_t, Digraph*);
-
-#endif //KAPPA_PAGERANK_H
