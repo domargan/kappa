adf157d2eab55ccbd3ab8cd7f7913b1c7457f8b3
Domagoj Margan <margan.domagoj@gmail.com>
2019-05-15 16:24:54 +0100

    naive_incremental_compute_edgelist experiments are updated and rewritten as a runtime instead

---
+++ src/core/runtimes/runtime_hard_barrier.h
@@ -0,0 +1,15 @@
+#ifndef KAPPA_RUNTIME_HARD_BARRIER_H
+#define KAPPA_RUNTIME_HARD_BARRIER_H
+
+#include "digraph.h"
+#include "read_from_disk/edgelist_to_edge_array.h"
+#include "thread_pool.h"
+#include "update.h"
+
+void run(Computation,
+        Updating,
+        Digraph*,
+        raw_edge_array_t&,
+        const std::vector<vertex_id_t> &);
+
+#endif //KAPPA_RUNTIME_HARD_BARRIER_H
