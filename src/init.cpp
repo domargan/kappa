b290c3fdde2e202d853b1ecd11489cb0a30f62c9
Domagoj Margan <margan.domagoj@gmail.com>
2019-05-15 16:25:31 +0100

    Updated init file with minor follow-up changes

--- src/init.cpp
+++ src/init.cpp
@@ -1,5 +1,8 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <insertions.h>
+#include <deletions.h>
+#include <runtimes/runtime_hard_barrier.h>
 
 #include "applications/dynamic/pagerank.h"
 #include "applications/dynamic/sssp.h"
@@ -52,21 +55,21 @@ int main(int argc, char *argv[]) {
     // ------------------------------------------------------------------------------------------ //
 
     // Choose dataset
+    // std::string dataset = "/home/dm1515/data/zachary.edgelist";
     // std::string dataset = "/home/dm1515/data/twitter-2010.txt";
     // std::string dataset = "/home/dm1515/data/higgs-social_network.edgelist";
     // std::string dataset = "/home/dm1515/data/higgs-social_network-shuffled.edgelist";
-    // std::string dataset = "/home/dm1515/data/higgs-social_network-shuffled-head1M";
-    // std::string dataset = "/home/dm1515/data/zachary.edgelist";
-    std::string dataset = "/home/dm1515/data/higgs-social_network-shuffled.edgelist";
-    // std::string dataset = "/home/leopold/Developer/graphs/zachary.edgelist";
-    // std::string dataset = "/home/leopold/Developer/graphs/higgs-social_network-shuffled.edgelist";
+    // std::string dataset = "/home/dm1515/data/sx-stackoverflow-no-stamps.txt";
+    std::string dataset = "/home/dm1515/data/wikipedia_link_en/out.wikipedia_link_en_11k";
+    //std::string dataset = "/home/dm1515/data/wikipedia_link_en/out.wikipedia_link_en";
 
     // Precomputed states for vertices in the core graph
-    // std::string core_states = "/home/leopold/Developer/graphs/zachary-states.txt";
     // std::string core_states = "/home/dm1515/kappa/utils/nx-validate/results/nx-sssp-zachary.txt";
-    // std::string core_states = "/home/dm1515/data/core-graphs/precomputed-states/nx-higgs-shuff-core-10k-pr.txt";
-    std::string core_states = "/home/dm1515/data/core-graphs/precomputed-states/nx-higgs-shuff-core-14M-sssp.txt";
-    // std::string core_states = "/home/leopold/Developer/graphs/nx-higgs-shuff-core-10k-sssp.txt";
+    //std::string core_states = "/home/dm1515/data/core-graphs/precomputed-states/nx-higgs-shuff-core-10k-pr.txt";
+    //std::string core_states = "/home/dm1515/data/core-graphs/precomputed-states/nx-higgs-shuff-core-14M-pr.txt";
+    // std::string core_states = "/home/dm1515/data/core-graphs/precomputed-states/nx-higgs-shuff-core-14M-sssp.txt";
+    // std::string core_states = "/home/dm1515/data/core-graphs/precomputed-states/nx-higgs-shuff-core-14M-wcc.txt";
+    // std::string core_states = "/home/dm1515/data/core-graphs/precomputed-states/sx-stackoverflow-no-stamps-core-63M.txt";
 
     // ------------------------------------------------------------------------------------------ //
 
@@ -83,18 +86,39 @@ int main(int argc, char *argv[]) {
     // ------------------------------------------------------------------------------------------ //
 
     // Set core graph size (#edges)
-    graph_size_t core_size = 14000000;
+    //graph_size_t core_size = 0;
+    //graph_size_t core_size = 14000000;
+    //graph_size_t core_size = 63000000;
+    //graph_size_t core_size = 189071210;
+    graph_size_t core_size = 10000;
+
 
     // TODO: Use user-defined functions
 
-    Computation computation;
-    computation.init_state = SSSP::init_state;
-    computation.on_activate = SSSP::on_activate;
-    computation.on_add_edge = SSSP::on_add_edge;
-    computation.on_remove_edge = SSSP::on_remove_edge;
-    
+    Updating updating;
+    updating.edge_insertion = Insertions::edge_insertion;
+    updating.edge_deletion = Deletions::edge_deletion;
+
+    Computation computation_pr;
+    computation_pr.init_state = PageRank::init_state;
+    computation_pr.on_activate = PageRank::on_activate;
+    computation_pr.on_add_edge = PageRank::on_add_edge;
+    computation_pr.on_remove_edge = PageRank::on_remove_edge;
+
+    Computation computation_sssp;
+    computation_sssp.init_state = SSSP::init_state;
+    computation_sssp.on_activate = SSSP::on_activate;
+    computation_sssp.on_add_edge = SSSP::on_add_edge;
+    computation_sssp.on_remove_edge = SSSP::on_remove_edge;
+
+    Computation computation_wcc;
+    computation_wcc.init_state = WCC::init_state;
+    computation_wcc.on_activate = WCC::on_activate;
+    computation_wcc.on_add_edge = WCC::on_add_edge;
+    computation_wcc.on_remove_edge = WCC::on_remove_edge;
+
     // Create a graph object
-    Digraph g = Digraph(max_vertex_num, batch_size, computation);
+    Digraph g = Digraph(max_vertex_num, batch_size, updating, computation_pr);
 
     // Populate the graph with core set of edges
     edge_array_to_digraph(&g, edge_array, 1, core_size);
@@ -108,14 +132,21 @@ int main(int argc, char *argv[]) {
     */
 
     // Load the precomputed states for vertices in the core graph
-    preload_states(&g, core_states, ' ', 1, core_size);
-    dump_vertex_states(&g, "preloaded-states-check.txt");
+    //preload_states(&g, core_states, ' ', 1, core_size);
+    //dump_vertex_states(&g, "preloaded-states-check.txt");
 
     // ------------------------------------------------------------------------------------------ //
 
     // Set a range of entries (lines) from the dataset, for updates to be applied to the graph
     graph_size_t beginning = core_size + 1;
-    graph_size_t end = 14500000;
+    //graph_size_t end = 10010000;
+    //graph_size_t end = 14000000;
+    //graph_size_t end = 14010000;
+    //graph_size_t end = 14100000;
+    //graph_size_t end = 14500000;
+    //graph_size_t end = 378142420;
+    //graph_size_t end = 189371211;
+    graph_size_t end = 11000;
 
     // Logically split the entires in the dataset by lines, splitting into batches
     std::vector<graph_size_t> split = dataset_to_batches(beginning, end, num_dataset_entries, batch_size);
@@ -124,12 +155,13 @@ int main(int argc, char *argv[]) {
     // Interchanging processes:
     // - updating the graph
     // - running incremental localised computation
-    naive_incremental_compute_edge_array(computation,
-                                       &g,
-                                       edge_array,
-                                       split);
+    run(computation_pr,
+            updating,
+            &g,
+            edge_array,
+            split);
 
-    dump_vertex_states(&g, "results.txt");
+    //dump_vertex_states(&g, "results.txt");
 
     // ------------------------------------------------------------------------------------------ //
 
