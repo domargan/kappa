a3fc69e61215acaca50a3e5f27ca9a7c3c1b884a
Domagoj Margan <margan.domagoj@gmail.com>
2019-08-23 14:32:14 +0100

    Added timer

--- src/preprocessor/edge_array_to_graph.cpp
+++ src/preprocessor/edge_array_to_graph.cpp
@@ -1,7 +1,11 @@
 #include <boost/progress.hpp>
+#include <boost/algorithm/string.hpp>
+
 #include "edge_array_to_graph.h"
 
 void edge_array_to_digraph(Digraph* g, raw_edge_array_t& edge_array, graph_size_t beggining_line, graph_size_t end_line) {
+    std::chrono::steady_clock::time_point begin_timer = std::chrono::steady_clock::now();
+
     std::cout << "Populating Digraph with entries from edge array..." << std::endl;
 
     boost::progress_display show_progress(end_line - beggining_line);
@@ -17,4 +21,8 @@ void edge_array_to_digraph(Digraph* g, raw_edge_array_t& edge_array, graph_size_
     }
 
     std::cout << "\nFinished populating.\n" << std::endl;
+
+    std::chrono::steady_clock::time_point end_timer = std::chrono::steady_clock::now();
+    float time = std::chrono::duration<float>(end_timer - begin_timer).count();
+    std::cout << "\n(CPU) DIGRAPH POPULATING TIME: " << time << std::endl;
 }
