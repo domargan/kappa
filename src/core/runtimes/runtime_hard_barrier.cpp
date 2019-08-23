ebee61c83a1502d1835cf91a817f1bdc739971ae
Domagoj Margan <margan.domagoj@gmail.com>
2019-08-23 14:31:03 +0100

    Switched back updating to single threaded out-of-task

--- src/core/runtimes/runtime_hard_barrier.cpp
+++ src/core/runtimes/runtime_hard_barrier.cpp
@@ -48,8 +48,27 @@ void run(Computation computation,
         std::chrono::system_clock::time_point system_begin_update = std::chrono::system_clock::now();
         std::chrono::steady_clock::time_point cpu_begin_update = std::chrono::steady_clock::now();
 
-        std::vector<Update> updates_in_chunk;
+        std::vector<Update> updates_in_chunk; // TODO: Make static array and do indexing instead of push_back
 
+        // Comment out from here...
+        for (graph_size_t j = start_line - 1; j < end_line - 1; j++) {
+            // std::cout << "READING EDGE " << updates[j][0] << " " << updates[j][1] << std::endl;
+
+            vertex_id_t src = updates[j][0];
+            vertex_id_t dst = updates[j][1];
+
+            //std::cout << src << " " << dst << std::endl;
+
+            g->add_edge(src, dst);
+
+            Update u(ADD, src, dst);
+            updates_in_chunk.push_back(u);
+        }
+        // ... to here for switching between task-based and no-task updates
+
+
+        // From here...
+        /*
         for (graph_size_t j = start_line - 1; j < end_line - 1; j++) {
             // std::cout << "READING EDGE " << updates[j][0] << " " << updates[j][1] << std::endl;
 
@@ -78,9 +97,12 @@ void run(Computation computation,
             GlobalScheduler::get_scheduler().submit(task);
         }
 
+
         GlobalScheduler::get_scheduler().barrier();
 
         GlobalScheduler::get_scheduler().task_counter = 0;
+        */
+        // ... to here
 
         // Write to file just to mark the iteration under which the barrier occurs
         //fs2 << std::chrono::duration<float>(std::chrono::system_clock::now() - GlobalScheduler::get_thread_pool().tp_start).count() << " " << GlobalScheduler::get_scheduler().iteration_counter << " " << g->get_order() << " " << g->get_size()<< " " << "-1" << " " << "-1" << std::endl;
@@ -97,6 +119,12 @@ void run(Computation computation,
         std::cout << "(SYS) UPDATE TIME FOR chunk " << std::fixed << i+1 << ": " << system_time_update << std::endl;
         std::cout << "(CPU) UPDATE TIME FOR chunk " << std::fixed << i+1 << ": " << cpu_time_update << std::endl;
 
+        // g->count_order();
+        graph_size_t order = g->get_order();
+        graph_size_t size = g->get_size();
+        std::cout << "Order: " <<  order << std::endl;
+        std::cout << "Size: " << size << std::endl;
+
         std::cout << "Computing CC..." << std::endl;
         std::chrono::steady_clock::time_point cpu_begin_cc = std::chrono::steady_clock::now();
         set_components_labels(g);
@@ -104,13 +132,6 @@ void run(Computation computation,
         std::cout << "Finished computing CC" << std::endl;
         float cpu_time_cc = std::chrono::duration<float>(cpu_end_cc - cpu_begin_cc).count();
         std::cout << "(CPU) CC TIME FOR chunk " << std::fixed << i+1 << ": " << cpu_time_cc << std::endl;
-
-
-        // g->count_order();
-        graph_size_t order = g->get_order();
-        graph_size_t size = g->get_size();
-        std::cout << "Order: " <<  order << std::endl;
-        std::cout << "Size: " << size << std::endl;
 
         std::cout << "Executing computations for " << updates_in_chunk.size() << " updates" << std::endl;
 
@@ -131,11 +152,9 @@ void run(Computation computation,
 
             GlobalScheduler::get_scheduler().submit(task);
 
-
             // CC-based scheduling here
             components_number_t src_component = task->g->get_component_label(u.src);
             components_number_t dst_component = task->g->get_component_label(u.dst);
-
 
             if (u.type == REMOVE && src_component != dst_component) {
 
