c6c6e995dd14493a099980cad038c03fcd67a6c8
Domagoj Margan <margan.domagoj@gmail.com>
2019-09-04 16:58:31 +0100

    Added dataset constants file

---
+++ data/datasets_constants.h
@@ -0,0 +1,12 @@
+#ifndef KAPPA_DATASETS_CONSTANTS_H
+#define KAPPA_DATASETS_CONSTANTS_H
+
+#include "datatypes.h"
+
+graph_size_t twitter2010_size = 1468365182;
+graph_size_t twitter2010_order = 41652230;
+
+graph_size_t higgs_size = 14855842;
+graph_size_t higgs_order = 456626;
+
+#endif //KAPPA_DATASETS_CONSTANTS_H
