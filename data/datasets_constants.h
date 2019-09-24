e00d75eadada6a2c2ba875524536dab31a36d4ac
Domagoj Margan <margan.domagoj@gmail.com>
2019-09-24 15:30:19 +0100

    Added twitter2010 core size constant

--- data/datasets_constants.h
+++ data/datasets_constants.h
@@ -5,6 +5,7 @@
 
 graph_size_t twitter2010_size = 1468365182;
 graph_size_t twitter2010_order = 41652230;
+graph_size_t twitter2010_core_size = 734182590;
 
 graph_size_t higgs_size = 14855842;
 graph_size_t higgs_order = 456626;
