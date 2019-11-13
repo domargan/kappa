8aa389202bedcc6b5c577e5b158e288336590d1a
Domagoj Margan <margan.domagoj@gmail.com>
2019-11-13 13:47:40 +0000

    Added update array type

--- src/core/datatypes.h
+++ src/core/datatypes.h
@@ -3,12 +3,20 @@
 
 #include <chrono>
 #include <cstdint>
+#include <array>
+#include <vector>
 
 typedef uint32_t vertex_id_t;
 
 typedef uint32_t graph_size_t;
 
 typedef uint32_t components_number_t;
+
+typedef std::vector<std::array<vertex_id_t, 2>>
+    raw_edge_array_t;  // format: [[src1,dst1],[src2,dst2],...]
+typedef std::vector<std::array<vertex_id_t, 3>>
+    raw_update_array_t;  // format: [[src1,dst1,{1,0}],[src2,dst2,{1,0}],...]
+                        // where 0 denotes deletion and 1 denotes addition
 
 typedef double state_t;
 
