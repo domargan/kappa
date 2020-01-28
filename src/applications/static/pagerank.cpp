c0ed2938ca1f75adb359ffba3ab4ecb00a850dde
Domagoj Margan <margan.domagoj@gmail.com>
2020-01-28 17:37:22 +0000

    Adjusted/changed static PR so we can use it to compute initial core graph values

--- src/applications/static/pagerank.cpp
+++ src/applications/static/pagerank.cpp
@@ -8,79 +8,54 @@
 // TODO: E.g. Is the considerd graph order for init values as large as the max
 // vertex num or as the current temporary order?
 
-/*
-void init_pr_values(Digraph* g){
-    //g->count_order();
+void init_pr_values(Digraph* g) {
+  float pr_init_val = (1.0 / g->get_order());
 
-    float pr_init_val = (1.0 / g->get_order());
+  std::cout << "Initial PageRank value is " << pr_init_val << std::endl;
 
-    std::cout << "Initial PageRank value is " << pr_init_val << std::endl;
+  graph_size_t max_order = g->get_max_order();
 
-    graph_size_t max_order = g->get_max_order();
-
-    for (graph_size_t i = 0; i < max_order; i++) {
-        g->update_state(i, pr_init_val);
+  for (graph_size_t i = 0; i < max_order; i++) {
+    if (g->has_vertex(i)) {
+      g->set_state(i, pr_init_val);
     }
-
-    g->finalize_states();
+  }
 }
 
 void pr_compute_single_vertex(vertex_id_t v, Digraph* g) {
-    float pr_neighbourhood_sum = 0.0;
-    for (auto neighbour : *(g->get_in_neighborhood(v))){
-        graph_size_t out_degree = g->get_out_degree(neighbour);
+  float pr_neighbourhood_sum = 0.0;
+  for (auto neighbour : (g->get_in_neighborhood(v))) {
+    graph_size_t out_degree = g->get_out_degree(neighbour);
 
-        if(out_degree > 0) {
-            pr_neighbourhood_sum += g->get_state(neighbour) / out_degree;
-        }
+    if (out_degree > 0) {
+      pr_neighbourhood_sum += g->get_state(neighbour) / out_degree;
     }
+  }
 
-    float pr = ((1.0 - DEFAULT_DAMPING_FACTOR) / g->get_order()) +
-(DEFAULT_DAMPING_FACTOR * pr_neighbourhood_sum);
+  float pr = ((1.0 - DEFAULT_DAMPING_FACTOR) / g->get_order()) +
+             (DEFAULT_DAMPING_FACTOR * pr_neighbourhood_sum);
 
-    g->update_state(v, pr);
+  g->set_state(v, pr);
 }
-*/
 
-/*
- * OBSOLETE!
- * Now doing this as part of run() in compute.cpp
+void pr_compute(Digraph* g) {
+  // g->count_order();
 
-void pr_compute(Digraph* g){
-    g->set_state_change_tolerance(DEFAULT_CONVERGENCE);
+  // Note: Don't forget to set initial PageRank vertex states outside (before)
+  // this function!
 
-    //g->count_order();
+  std::cout << "Starting PageRank computation (maximum " << 10 << " iterations)..." << std::endl;
 
-    // Note: Don't forget to set initial PageRank vertex states outside (before)
-this function!
+  graph_size_t max_order = g->get_max_order();
 
-    //std::cout << "Starting PageRank computation (maximum " <<
-DEFAULT_MAX_ITERATIONS << " iterations)..." << std::endl;
-
-    graph_size_t max_order = g->get_max_order();
-
-    int num_iterations = 0;
-    g->state_change_monitor = true;
-
-    while(g->state_change_monitor && num_iterations < DEFAULT_MAX_ITERATIONS) {
-        g->state_change_monitor = false;
-
-        for (graph_size_t i = 0; i < max_order; i++) {
-            if(g->has_vertex(i)){
-                pr_compute_single_vertex(i, g);
-            }
-        }
-
-        g->finalize_states();
-
-        num_iterations++;
+  int num_iterations = 0;
+  while (num_iterations < 10) {
+    for (graph_size_t i = 0; i < max_order; i++) {
+      if (g->has_vertex(i)) {
+        pr_compute_single_vertex(i, g);
+      }
     }
 
-    if(num_iterations == DEFAULT_MAX_ITERATIONS) {
-    //    std::cout << "PageRank stopped after " << num_iterations << "
-iterations" << std::endl; } else {
-    //    std::cout << "PageRank converged after " << num_iterations << "
-iterations" << std::endl;
-    }
+    num_iterations++;
+  }
 }
- */
--- src/applications/static/pagerank.h
+++ src/applications/static/pagerank.h
@@ -7,5 +7,6 @@ const float DEFAULT_DAMPING_FACTOR = 0.85;
 
 void init_pr_values(Digraph*);
 void pr_compute_single_vertex(vertex_id_t, Digraph*);
+void pr_compute(Digraph*);
 
 #endif  // KAPPA_PAGERANK_H
